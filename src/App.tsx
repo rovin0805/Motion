cc7d5d53eb8a8629903f5eb54d8990a37d9d52db
최은정 <qhdrn67@naver.com>
2022-01-27 10:21:14 +0900

    Gestures part One

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,22 @@ const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
 `;
 
 const Box = styled(motion.div)`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 15px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Box2 = styled(motion.div)`
+  display: grid;
   width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 0.2);
@@ -57,15 +66,31 @@ const circleVariants: Variants = {
   },
 };
 
+const box2Variants: Variants = {
+  hover: { scale: 1.2, rotateZ: 90 },
+  click: { scale: 1, borderRadius: '100px' },
+  drag: { backgroundColor: 'rgb(46, 204, 113)', transition: { duration: 5 } },
+};
+
 function App() {
   return (
     <Wrapper>
+      {/* variants */}
       <Box variants={boxVariants} initial='start' animate='end'>
         <Circle variants={circleVariants} />
         <Circle variants={circleVariants} />
         <Circle variants={circleVariants} />
         <Circle variants={circleVariants} />
       </Box>
+
+      {/* gestures */}
+      <Box2
+        variants={box2Variants}
+        whileHover='hover'
+        whileTap='click'
+        drag
+        whileDrag='drag'
+      />
     </Wrapper>
   );
 }
